ff07a720b872b779afe437781c8f7382727348ab
NitkaNikita <waleryvvs1@gmail.com>
2022-09-13 20:31:01 +0300

    init config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
+#![allow(dead_code)] //
+
+use core::panic::PanicInfo;
+mod vga_buffer;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = "GIGACHAD".as_bytes();
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+    
+    loop {}
 }
